999abc35d022f01f46704251dfeed47a003a5cf9
Tércio <terciojorge@gmail.com>
2014-04-29 16:54:45 -0300

    Usuario e Aluno atualizados

--- src/main/java/br/quixada/ufc/npi/model/Usuario.java
+++ src/main/java/br/quixada/ufc/npi/model/Usuario.java
@@ -1,5 +1,6 @@
 package br.quixada.ufc.npi.model;
 
+import java.util.Date;
 import java.util.List;
 
 import javax.persistence.Column;
@@ -10,7 +11,10 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.persistence.UniqueConstraint;
 
 @Entity
@@ -33,6 +37,158 @@ public class Usuario {
 	
 	@Column(nullable = false)
 	private boolean habilitado;
+
+	
+	@Column(nullable = false)
+	private String email;
+	
+	private String nome;
+	
+	@Column(name = "datanascimento", columnDefinition="DATE") 
+	@Temporal(TemporalType.TIMESTAMP) 
+	private Date dataNascimento;
+	
+	private String nacionalidade;
+	
+	private String naturalidade;
+	
+	public enum Uf{
+		AC, AL, AM, AP, BA, CE, DF, ES, GO, MA, MG, MS, MT, PA, PB, PE, PI, PR, RJ, RN, RO, RR, RS, SC, SE, SP, TO;
+	}
+	
+	private String sexo;
+	
+	private String cpf;
+	
+	private String rg;
+	
+	private String telefone;
+	
+	private String estadoCivil;
+	
+		
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getNacionalidade() {
+		return nacionalidade;
+	}
+
+	public void setNacionalidade(String nacionalidade) {
+		this.nacionalidade = nacionalidade;
+	}
+
+	public String getNaturalidade() {
+		return naturalidade;
+	}
+
+	public void setNaturalidade(String naturalidade) {
+		this.naturalidade = naturalidade;
+	}
+
+	
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEstadoCivil() {
+		return estadoCivil;
+	}
+
+	public void setEstadoCivil(String estadoCivil) {
+		this.estadoCivil = estadoCivil;
+	}
+
+	public List<Papel> getPapeis() {
+		return papeis;
+	}
+
+	public void setPapeis(List<Papel> papeis) {
+		this.papeis = papeis;
+	}
+	
+	
+	public List<Aluno> getAlunos() {
+		return alunos;
+	}
+
+	public void setAlunos(List<Aluno> alunos) {
+		this.alunos = alunos;
+	}
+
+	
+	@OneToMany
+	private List<Aluno> alunos;
+	
+	public List<Servidor> getServidores() {
+		return servidores;
+	}
+
+	public void setServidores(List<Servidor> servidores) {
+		this.servidores = servidores;
+	}
+
+	
+	@OneToMany
+	private List<Servidor> servidores;
+	
 
 	@ManyToMany
 	@JoinTable(name = "papel_usuario", joinColumns = @JoinColumn(name = "usuario_id"), inverseJoinColumns = @JoinColumn(name = "papel_id"))
