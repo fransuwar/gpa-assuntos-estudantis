b4d8b81c16972e8802203fca7fe2238a0f4edddb
Cinthia <cinthia.mmh@gmail.com>
2014-09-30 14:05:20 -0300

    correção de bugs

--- aplicacoes/gpa-mae/src/main/java/br/com/ufc/quixada/npi/gpa/service/IniciacaoAcademicaService.java
+++ aplicacoes/gpa-mae/src/main/java/br/com/ufc/quixada/npi/gpa/service/IniciacaoAcademicaService.java
@@ -3,5 +3,5 @@ package br.com.ufc.quixada.npi.gpa.service;
 import br.com.ufc.quixada.npi.gpa.model.QuestionarioIniciacaoAcademica;
 
 public interface IniciacaoAcademicaService extends GenericService<QuestionarioIniciacaoAcademica> {
-
+	
 }
