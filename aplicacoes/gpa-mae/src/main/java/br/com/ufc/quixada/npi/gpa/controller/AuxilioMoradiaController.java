64be851877f703fb7965e3c352e973d618ba39e2
Adeilson <adeilsonaragao@hotmail.com>
2014-11-24 17:09:59 -0300

    Adição da biblioteca do Logger4j

--- aplicacoes/gpa-mae/src/main/java/br/com/ufc/quixada/npi/gpa/controller/AuxilioMoradiaController.java
+++ aplicacoes/gpa-mae/src/main/java/br/com/ufc/quixada/npi/gpa/controller/AuxilioMoradiaController.java
@@ -11,6 +11,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
