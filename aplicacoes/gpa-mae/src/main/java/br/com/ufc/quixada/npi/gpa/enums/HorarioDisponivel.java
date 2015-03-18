4e1d1504ad84b1d1e29f7c44f3ff668d477a6a9c
root <robertojunior085@gmail.com>
2015-03-18 15:13:34 -0300

    eliminação de redudância de enums na classe quest ini academ

--- aplicacoes/gpa-mae/src/main/java/br/com/ufc/quixada/npi/gpa/enums/HorarioDisponivel.java
+++ aplicacoes/gpa-mae/src/main/java/br/com/ufc/quixada/npi/gpa/enums/HorarioDisponivel.java
@@ -1,12 +1,15 @@
 package br.com.ufc.quixada.npi.gpa.enums;
 
-public enum HorarioDisponivel{
+public enum HorarioDisponivel {
 	Manhã("Manhã"), Tarde("Tarde"), Noite("Noite");
+	
 	private String nome;
-	private HorarioDisponivel(String nome){
+
+	private HorarioDisponivel(String nome) {
 		this.nome = nome;
 	}
-	public String getNome(){
+
+	public String getNome() {
 		return nome;
 	}
 }
