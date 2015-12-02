7543071d818d22f0776b0a3429eb2fe1db6afd0e
CaioPessoa <caiocavalcante14@gmail.com>
2015-12-02 14:29:43 -0300

    Ajustes Necessarios para o listar seleção voltar a funcionar

--- aplicacoes/gpa-mae/src/main/java/br/ufc/quixada/npi/gpa/controller/SelecaoController.java
+++ aplicacoes/gpa-mae/src/main/java/br/ufc/quixada/npi/gpa/controller/SelecaoController.java
@@ -58,32 +58,6 @@ public class SelecaoController {
 	private QuestionarioAuxMoradiaService auxService;
 
 	
-	@RequestMapping(value = { "/listar" }, method = RequestMethod.GET)
-	public String listar(ModelMap model, HttpServletRequest request, Authentication auth) {
-		
-		List<Selecao> selecoes = this.selecaoService.find(Selecao.class);
-
-		if (request.isUserInRole("DISCENTE")) {
-
-			
-			Aluno aluno = this.alunoService.getAlunoComInscricoesCpf(auth.getName());
-			
-			model.addAttribute("selecoes", selecoes);
-			model.addAttribute("aluno", aluno);
-			model.addAttribute("inic_acad", TipoBolsa.INIC_ACAD);
-			model.addAttribute("aux_mor", TipoBolsa.AUX_MOR);
-			
-		} else {
-			
-			model.addAttribute("selecoes", selecoes);
-			model.addAttribute("tipoBolsa", TipoBolsa.values());
-			model.addAttribute("inic_acad", TipoBolsa.INIC_ACAD);
-			model.addAttribute("aux_mor", TipoBolsa.AUX_MOR);
-		}
-		
-		return PAGINA_LISTAR_SELECAO;
-	}
-	
 
 	@RequestMapping(value = { "detalhes/{idSelecao}" }, method = RequestMethod.GET)
 	public String getInformacoes(@PathVariable("idSelecao") Integer idSelecao, Model model,
