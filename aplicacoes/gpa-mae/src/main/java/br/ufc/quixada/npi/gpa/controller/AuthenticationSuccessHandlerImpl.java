145cb5ce310d3730f51f36336434a254e668c6f0
CaioPessoa <caiocavalcante14@gmail.com>
2015-12-07 17:09:37 -0300

    Criar e definir Perfil de Administrador e suas funções

--- aplicacoes/gpa-mae/src/main/java/br/ufc/quixada/npi/gpa/controller/AuthenticationSuccessHandlerImpl.java
+++ aplicacoes/gpa-mae/src/main/java/br/ufc/quixada/npi/gpa/controller/AuthenticationSuccessHandlerImpl.java
@@ -59,6 +59,8 @@ public class AuthenticationSuccessHandlerImpl implements
 			else if (grantedAuthority.getAuthority().equalsIgnoreCase(DISCENTE)){
 				
 				return "/aluno/selecao/listar";
+			}else if (grantedAuthority.getAuthority().equalsIgnoreCase(ADMINISTRADOR)){
+				return "/admin/listar";
 			}
 		}
 		
--- aplicacoes/gpa-mae/src/main/java/br/ufc/quixada/npi/gpa/utils/Constants.java
+++ aplicacoes/gpa-mae/src/main/java/br/ufc/quixada/npi/gpa/utils/Constants.java
@@ -119,5 +119,7 @@ public class Constants {
 	public static final String DISCENTE = "DISCENTE";
 	
 	public static final String COORDENADOR_ASSUNTOS_ESTUDANTIS = "COORDENADOR_ASSUNTO_ESTUDANTIS";
+	
+	public static final String 	ADMINISTRADOR =	"ADMINISTRADOR_GPA";
 
 }
