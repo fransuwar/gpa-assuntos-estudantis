0c6a74fe18f7f31f8de4a16a728f17f3f5cc676f
natalialionel <natalia.lionel@alu.ufc.br>
2016-01-06 14:38:18 -0300

    Correção de mensagem informando o sucesso ao adicionar membro em comissão

--- aplicacoes/gpa-mae/src/main/java/br/ufc/quixada/npi/gpa/utils/Constants.java
+++ aplicacoes/gpa-mae/src/main/java/br/ufc/quixada/npi/gpa/utils/Constants.java
@@ -115,7 +115,7 @@ public class Constants {
 	
 	public static final String MENSAGEM_SUCESSO_DOWNLOAD_DOCUMENTO	= "Download do Documento realizado com sucesso";
 	
-	public static final String MENSAGEM_SUCESSO_COMISSAO_FORMADA	= "Informe pelo menos um membro.";
+	public static final String MENSAGEM_SUCESSO_COMISSAO_FORMADA	= "Membro inserido com sucesso.";
 	
 	public static final String MENSAGEM_SUCESSO_MEMBRO_EXCLUIDO		= "Membro excluído com sucesso.";
 	
