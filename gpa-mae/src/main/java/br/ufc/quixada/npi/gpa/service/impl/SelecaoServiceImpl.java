3188272e4ad04172a5dec6366800f434e6a95458
Daniel Farias <danielfarias09@hotmail.com>
2016-04-14 15:55:35 -0300

    resolvido erro de quando não havia nenhuma seleção

--- gpa-mae/src/main/java/br/ufc/quixada/npi/gpa/service/impl/SelecaoServiceImpl.java
+++ gpa-mae/src/main/java/br/ufc/quixada/npi/gpa/service/impl/SelecaoServiceImpl.java
@@ -42,6 +42,11 @@ public class SelecaoServiceImpl extends GenericServiceImpl<Selecao> implements S
 	public Integer getUltimoSequencialPorAno(Selecao selecao) {
 		List<Integer> listSequencial = find(QueryType.JPQL,"select max(s.sequencial)from Selecao as s where s.tipoSelecao = :tipoSelecao and s.ano = :ano",
 				new SimpleMap<String,Object>("tipoSelecao", selecao.getTipoSelecao(), "ano",selecao.getAno()));
+		
+		if (listSequencial == null || listSequencial.get(0) == null) {
+			return 1;
+		}else{
 		return listSequencial.get(0)+1;
+		}
 	}
 }
