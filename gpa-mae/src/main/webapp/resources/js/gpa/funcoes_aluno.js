66bcf39b4dfc379b913af90cc78817d85960d3a7
Daniel Filho <dielfilho@hotmail.com>
2016-05-02 16:28:15 -0300

    Refatoração do javascript e adição da lib de estados e cidades.

---
+++ gpa-mae/src/main/webapp/resources/js/gpa/funcoes_aluno.js
@@ -0,0 +1,288 @@
+/*
+ * Esse aquivo contém todas as funções 
+ * javascript das páginas acessadas por alunos.
+ */ 
+
+var FormularioAuxilio = function() {
+    var self = this;
+    var $formElement = $("#questionarioAuxilio");
+    
+    /*
+     * Método que inicia todos os 
+     * códigos do formulario de auxilio.
+     */
+    self.init = function(){
+    	$formElement.validate();
+    	
+    	//Iniciando plugin step.
+    	self.initStep();
+    	self.initMascaras();
+    	self.initDivCursinho();
+    	self.initDivMoraComOutros();
+    	self.initSelectEnsinoMedio();
+    	self.initSelectEstadoCidade();
+    	self.initSelectSituacaoImovel();
+    	self.initBotaoFinalizarInscricao();
+    	self.initSelectEnsinoFundamental();
+    	self.initSelectParentescoVeiculos();
+    	self.initSelectParentescoImovelRural();
+       	
+    }
+    
+    self.initStep = function() {
+        $formElement.steps({
+            headerTag: "h3",
+            bodyTag: "section",
+            transitionEffect: "fade",
+            labels: {
+                cancel: "Cancelar",
+                current: "Passo atual:",
+                pagination: "Paginação",
+                finish: "Finalizar",
+                next: "Próximo",
+                previous: "Anterior",
+                loading: "Carregando ..."
+            },
+            autoFocus: true,
+            onStepChanging: function(event, currentIndex, newIndex) {
+                if (currentIndex < newIndex) {
+                	$formElement.validate().settings.ignore = ":disabled,:hidden";
+                    return $formElement.valid();
+                }
+                return true;
+            },
+            onFinishing: function(event, currentIndex) {
+            	$formElement.validate().settings.ignore = ":disabled,:hidden";
+                return $formElement.valid();
+            },
+            onFinished: function(event, currentIndex) {
+            	$formElement.submit();
+            }
+        });
+    }
+    
+	/*
+	 * Todas as funções do select de situação
+	 * de imóvel.
+	 */
+    self.initSelectSituacaoImovel = function(){
+    	var $select = $("#situacaoImovel");
+
+    	var $divValorMensal = $("#div-valor-mensal");
+    	var $inputValorMensal = $("#valorMensalFinanciamento");
+    	/*	
+         * 	Essa função é chamada quando o usuário seleciona 
+         * 	alguma opção no select de Situação do Imóvel. 
+         */
+    	$select.on("change", function(){
+    		var valorSelecionado = $select.find("option:selected").text();
+
+    		if(valorSelecionado == "Financiado"){
+    			$divValorMensal.removeClass("hidden");
+    		}else{
+    			$divValorMensal.addClass("hidden");
+    			$inputValorMensal.val("0");
+    		}
+    	});
+    }
+ 
+    /*
+     * Essa função é responsável por validar a extenção do arquivo
+     * que o usuário envia no upload.
+     * As extenções são: jpeg, jpg e png.
+     * 
+     * Se o valor do input for igual a "" então o usuário não fez upload
+     * de nenhuma imagem, logo é retornado true para não aparecer a mensagem
+     * de foto com formato inválido.
+     */
+    self.isExtencaoFotoValida = function($input){
+    	var extencoes = ["jpeg", "jpg", "png"];
+    	var fileName = $input.val();
+
+    	if(fileName == "")
+    		return true;
+
+    	var extencaoFoto = fileName.split(".")[1] ? $input.val().split(".")[1] : "";
+    	var res = extencoes.some(function(extencao){
+    		return extencao == extencaoFoto;
+    	});
+    	return res;
+
+    }
+
+    /*
+     * Todas as funções envolvendo
+     * o botão de finalizar a inscrição
+     * estão nesse método.
+     */
+    self.initBotaoFinalizarInscricao = function(){
+    	var $button = $("#form-btn");
+    	var $input = $("#input-foto3x4");
+    	var $spanError = $("#span-error-foto");
+    	$spanError.text("");
+    	$button.on("click", function(event){
+    		if(!isExtencaoFotoValida($input)){
+    			$spanError.text("Foto com extensão inválida!");
+    		}
+    	});
+    }
+    
+    /*
+     * Todas as funções envolvendo
+     * o select grauParentescoImovelRural
+     * estão nesse método
+     */
+    self.initSelectParentescoImovelRural = function(){
+    	var $select = $("#grauParentescoImovelRural");
+    	$select.change(function(){
+    		var $selectOutroGrauParentesco = $("#outroGrauParentescoImovelRural");
+    		var $labelOutroGrauParentesco = $("#labelOutroGrauParentescoImovelRural");
+    		if($(this).val() == "OUTROS"){
+    			$selectOutroGrauParentesco.val("");
+    			$selectOutroGrauParentesco.css("display", "block");
+    			$labelOutroGrauParentesco.css("display", "block");
+    		}else{
+    			$selectOutroGrauParentesco.css("display", "none");
+    			$labelOutroGrauParentesco.css("display", "none");
+    		}
+    	});
+    }
+    
+    /*
+     * Todas as funções envolvendo o select de
+     * grau de parentesco de veículos estão nesse
+     * método. 
+     */
+    self.initSelectParentescoVeiculos = function(){
+    	var $select = $("#grauParentescoVeiculos"); 
+    	var $outroGrauParentesco = $("#outroGrauParentescoVeiculos");
+    	var $labelOutroParentesco = $("#labelOutroGrauParentescoVeiculos");
+    	
+    	var $outroGrauImovelRural = $("#outroGrauParentescoImovelRural")
+    	var $labelGrauImovelRural = $("#labelOutroGrauParentescoImovelRural");
+    	
+	    $select.change(function(){
+	    	if($(this).val() == "OUTROS"){
+				$outroGrauParentesco.val("");
+				$outroGrauParentesco.css("display", "block");
+				$labelOutroParentesco.css("display", "block");
+			}else{
+				$outroGrauParentesco.css("display", "none");
+				$labelOutroParentesco.css("display", "none");
+			}
+		});
+	    
+	    if($select.val() == "OUTROS"){
+	    	$outroGrauParentesco.css("display", "block");
+	    	$labelOutroParentesco.css("display", "block");
+
+	    	$outroGrauImovelRural.css("display", "block");
+	    	$labelGrauImovelRural.css("display", "block");
+		}else{
+			$outroGrauParentesco.css("display", "none");
+			$labelOutroParentesco.css("display", "none");
+
+			$outroGrauImovelRural.css("display", "none");
+			$labelGrauImovelRural.css("display", "none");
+		}
+	    
+    }
+    
+    self.initDivCursinho = function(){
+    	var $divNomeCursinho = $("#nome_cursinho");
+    	
+    	$divNomeCursinho.hide();
+    	
+    	$("#cursinho").click(function() {
+    		if($(this).is(":checked"))
+    			$divNomeCursinho.show();
+    		else       
+    			$divNomeCursinho.hide();
+    	});
+
+    }
+    
+    /*
+     * Todas as mascaras do formulário 
+     * estão nesse método.
+     */
+    self.initMascaras = function(){
+    	$("#valorMensalFinanciamento").mask("###0000000.00", {reverse: true});
+    	$("#areaPropriedadeRural").mask("#####0.00", {reverse: true});
+    	$("#rendaMensal").maskMoney({showSymbol:true, symbol:"R$", decimal:".", thousands:"."});
+    }
+
+    /*
+     * Todas as funções do select bolsa
+     * particular ensino médio.
+     */
+    self.initSelectEnsinoMedio = function(){
+    	var $divBolsaParticularMedio = $("#percentualParticularMedio"); 
+    	$divBolsaParticularMedio.hide();
+    	
+    	var $select = $("#ensinoMedio");
+    	
+    	//Mostrar o percentual de bolsa quando clicar na opção : "Particular com Bolsa"
+    	$select.change(function() {
+    		var option = $(this).find("option:selected").text();
+			if(option == "Particular com Bolsa"){
+				$divBolsaParticularMedio.show();
+			} else {
+				$divBolsaParticularMedio.hide();
+			}	
+    	});
+
+    }
+    
+    self.initSelectEnsinoFundamental = function(){
+    	var $divBolsaParticularFundamental = $("#percentualParticularFundamental"); 
+    	$divBolsaParticularFundamental.hide();
+    	
+    	var $select = $("#ensinoFundamental");
+    	
+    	$select.change(function() {
+    		var option = $(this).find("option:selected").text();
+			if(option == "Particular com Bolsa"){
+				$divBolsaParticularFundamental.show();
+			} else {
+				$divBolsaParticularFundamental.hide();
+			}
+    	});
+
+    }
+    
+    /*
+     * Todas as funções na div
+     * "mora com outros" estão 
+     * nesse método.
+     */
+    self.initDivMoraComOutros = function(){
+    	var $divMoraComOutros = $("#mora-com-outros"); 
+    	
+    	$divMoraComOutros.hide();
+		$("#comQuemMora7").change(function () {
+			if ($(this).prop("checked")) {
+				$divMoraComOutros.show();
+			}else {
+				$divMoraComOutros.hide();                                                                    
+			}
+		});
+    }
+    
+    /*
+     * Esse método inicia a biblioteca para
+     * preencher os selects de estado e cidade
+     * Estado e cidade de origem,
+     * Estado e cidade atual.
+     */
+    self.initSelectEstadoCidade = function(){
+    	new StateCityLib().init("estado-endereco", "cidade-endereco");
+    	new StateCityLib().init("estado-origem", "cidade-origem");
+    }
+}
+
+var formAuxilio = new FormularioAuxilio();
+
+$(document).ready(function(){
+	formAuxilio.init();	
+});
