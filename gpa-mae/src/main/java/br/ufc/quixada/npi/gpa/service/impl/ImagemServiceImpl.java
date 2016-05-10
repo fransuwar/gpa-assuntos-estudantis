ecfc192a67ce58ed1478465c24b2d8bf08e44fc9
Alan Martins <alanfortlink@gmail.com>
2016-05-10 15:36:59 -0300

    Adicionando classe ImagemService

---
+++ gpa-mae/src/main/java/br/ufc/quixada/npi/gpa/service/impl/ImagemServiceImpl.java
@@ -0,0 +1,12 @@
+package br.ufc.quixada.npi.gpa.service.impl;
+
+import javax.inject.Named;
+
+import br.ufc.quixada.npi.gpa.model.Imagem;
+import br.ufc.quixada.npi.gpa.service.ImagemService;
+import br.ufc.quixada.npi.service.impl.GenericServiceImpl;
+
+@Named
+public class ImagemServiceImpl extends GenericServiceImpl<Imagem> implements ImagemService{
+
+}
