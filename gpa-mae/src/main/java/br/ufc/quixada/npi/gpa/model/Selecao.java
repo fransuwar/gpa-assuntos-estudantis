75b74d025bdba37f24794f89c53c65fc63697c22
Flávio Barros <juniorbarros3@gmail.com>
2016-06-21 14:05:50 -0300

    remoção de import não utilizado

--- gpa-mae/src/main/java/br/ufc/quixada/npi/gpa/model/Selecao.java
+++ gpa-mae/src/main/java/br/ufc/quixada/npi/gpa/model/Selecao.java
@@ -2,7 +2,6 @@ package br.ufc.quixada.npi.gpa.model;
 
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
