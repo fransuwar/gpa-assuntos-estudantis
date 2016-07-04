468a701c80856f16be9659cb36eceba2e6996a67
romulo <romulocosta100@gmail.com>
2016-07-04 17:09:41 -0300

    Correções

--- gpa-mae/src/main/webapp/resources/js/gpa/funcoes_aluno.js
+++ gpa-mae/src/main/webapp/resources/js/gpa/funcoes_aluno.js
@@ -44,24 +44,19 @@ var FormularioAuxilio = function() {
 	};
 	
 	self.filtroFoto = function(){
-		
 		$( "#input-foto3x4" ).change(function (e)  {
-			  console.log("foi");
-			  var filename = $('#input-foto3x4').val();
-			  var fileExtension = filename.substring(filename.lastIndexOf(".") + 1, filename.length);
-			  if(fileExtension==="jpg" || fileExtension==="png"){
-				  return true;
-				  
-			  }else{
-				  $('#input-foto3x4').val("");
-				  return false;
-			  }
-			  
-				  
-			  
-			});
-		
-	
+			var filename = $("#input-foto3x4").val();
+			var fileExtension = filename.substring(filename.lastIndexOf(".") + 1, filename.length);
+			if(fileExtension==="jpg" || fileExtension==="png"){
+				return true;
+
+			}else{
+				$("#input-foto3x4").val("");
+				return false;
+			}
+
+		});
+
 	};
 	
 	
