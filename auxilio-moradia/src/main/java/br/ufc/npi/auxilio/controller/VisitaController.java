b4aba207d73e645330fbc036642978c575831afc
Mariana <mariana.aparecida14@gmail.com>
2017-05-08 17:34:59 -0300

    Pagina Visita Domiciliar

--- auxilio-moradia/src/main/java/br/ufc/npi/auxilio/controller/VisitaController.java
+++ auxilio-moradia/src/main/java/br/ufc/npi/auxilio/controller/VisitaController.java
@@ -1,9 +1,16 @@
 package br.ufc.npi.auxilio.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("visita")
 public class VisitaController {
+	
+	@GetMapping("/form")
+	public String formVisitaTeste(){
+		
+		return "selecao/visita_domiciliar";
+	}
 }
