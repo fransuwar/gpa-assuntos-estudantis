4f0be28d721f31278ffefde59ff38b5db8c935f9
Mariana <mariana.aparecida14@gmail.com>
2017-05-09 17:31:25 -0300

    Adicionando metodos no controller de VisitaDomiciliar, links para teste, alteração no breadcrumb css na pagina visita-domiciliar

--- auxilio-moradia/src/main/java/br/ufc/npi/auxilio/controller/VisitaController.java
+++ auxilio-moradia/src/main/java/br/ufc/npi/auxilio/controller/VisitaController.java
@@ -1,16 +1,52 @@
 package br.ufc.npi.auxilio.controller;
 
+import static br.ufc.npi.auxilio.utils.Constants.PERMISSAO_COORDENADOR;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.ufc.npi.auxilio.enums.Resultado;
+import br.ufc.npi.auxilio.enums.TipoSelecao;
+import br.ufc.npi.auxilio.model.Aluno;
+import br.ufc.npi.auxilio.model.Inscricao;
+import br.ufc.npi.auxilio.model.Selecao;
+import br.ufc.npi.auxilio.service.AlunoService;
+import br.ufc.npi.auxilio.service.InscricaoService;
 
 @Controller
 @RequestMapping("visita")
 public class VisitaController {
 	
-	@GetMapping("/form")
-	public String formVisitaTeste(){
+	@Autowired
+	private InscricaoService inscricaoService;
+
+	@Autowired
+	private AlunoService alunoService;
+	
+	@PreAuthorize(PERMISSAO_COORDENADOR)
+	@GetMapping("/detalhes/{inscricao}")
+	public String detalhes(@PathVariable Inscricao inscricao,  Authentication auth, Model model){
+		Aluno aluno = alunoService.buscarPorCpf(auth.getName());
+		Inscricao inscricaoBanco = inscricaoService.get(aluno, inscricao.getSelecao());
+		model.addAttribute("inscrição", inscricaoBanco != null ? inscricaoBanco.getId() : null);
+		model.addAttribute("selecao", inscricao.getSelecao());
+		model.addAttribute("resultados", Resultado.values());
+		return "selecao/visita-domiciliar";
+	}
+	
+	@PreAuthorize(PERMISSAO_COORDENADOR)
+	@GetMapping("/cadastrar")
+	public String cadastrarVisitaForm(Model model) {
+		model.addAttribute("acao", "cadastrar");
+		model.addAttribute("opcoesTipoSelecao", TipoSelecao.values());
+		model.addAttribute("selecao", new Selecao());
 		
-		return "selecao/visita_domiciliar";
+		return "selecao/cadastrar-visita";
 	}
 }
