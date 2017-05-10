592bcd9ce3c058339447dc846d0c9cd76dc30eab
Mariana <mariana.aparecida14@gmail.com>
2017-05-10 15:35:27 -0300

    Breadcrumb css, listagem dos resultados.

--- auxilio-moradia/src/main/java/br/ufc/npi/auxilio/controller/VisitaController.java
+++ auxilio-moradia/src/main/java/br/ufc/npi/auxilio/controller/VisitaController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.ufc.npi.auxilio.enums.Resultado;
@@ -16,8 +17,11 @@ import br.ufc.npi.auxilio.enums.TipoSelecao;
 import br.ufc.npi.auxilio.model.Aluno;
 import br.ufc.npi.auxilio.model.Inscricao;
 import br.ufc.npi.auxilio.model.Selecao;
+import br.ufc.npi.auxilio.model.Servidor;
+import br.ufc.npi.auxilio.model.VisitaDomiciliar;
 import br.ufc.npi.auxilio.service.AlunoService;
 import br.ufc.npi.auxilio.service.InscricaoService;
+import br.ufc.npi.auxilio.service.ServidorService;
 
 @Controller
 @RequestMapping("visita")
@@ -29,20 +33,27 @@ public class VisitaController {
 	@Autowired
 	private AlunoService alunoService;
 	
+	@Autowired
+	private ServidorService servidorService;
+	
 	@PreAuthorize(PERMISSAO_COORDENADOR)
-	@GetMapping("/detalhes/{inscricao}")
-	public String detalhes(@PathVariable Inscricao inscricao,  Authentication auth, Model model){
-		Aluno aluno = alunoService.buscarPorCpf(auth.getName());
+	@GetMapping("/cadastrar/{inscricao}")
+	public String cadastrarVisitaForm(@PathVariable Inscricao inscricao, Authentication auth, Model model){
+		Aluno aluno = alunoService.buscarPorCpf(inscricao.getAluno().getPessoa().getCpf());
+		Servidor servidor =  servidorService.getByCpf(auth.getName());
+		
 		Inscricao inscricaoBanco = inscricaoService.get(aluno, inscricao.getSelecao());
 		model.addAttribute("inscrição", inscricaoBanco != null ? inscricaoBanco.getId() : null);
 		model.addAttribute("selecao", inscricao.getSelecao());
 		model.addAttribute("resultados", Resultado.values());
+		model.addAttribute("visita", new VisitaDomiciliar());
+		model.addAttribute("tiposDeDocumento", inscricao.getSelecao().getDocumentos());
 		return "selecao/visita-domiciliar";
 	}
 	
 	@PreAuthorize(PERMISSAO_COORDENADOR)
-	@GetMapping("/cadastrar")
-	public String cadastrarVisitaForm(Model model) {
+	@PostMapping("/cadastrar/{inscricao}")
+	public String cadastrarVisita(Model model) {
 		model.addAttribute("acao", "cadastrar");
 		model.addAttribute("opcoesTipoSelecao", TipoSelecao.values());
 		model.addAttribute("selecao", new Selecao());
