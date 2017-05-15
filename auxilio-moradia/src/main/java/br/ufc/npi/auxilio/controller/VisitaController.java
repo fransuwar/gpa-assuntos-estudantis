6cf536f3975ea52c1aa6605201dd5ee2a47b0729
Mariana <mariana.aparecida14@gmail.com>
2017-05-15 17:31:04 -0300

    Adicionado campos de arquivos a página.

--- auxilio-moradia/src/main/java/br/ufc/npi/auxilio/controller/VisitaController.java
+++ auxilio-moradia/src/main/java/br/ufc/npi/auxilio/controller/VisitaController.java
@@ -8,17 +8,17 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.ufc.npi.auxilio.enums.Resultado;
-import br.ufc.npi.auxilio.enums.TipoSelecao;
 import br.ufc.npi.auxilio.model.Aluno;
 import br.ufc.npi.auxilio.model.Inscricao;
-import br.ufc.npi.auxilio.model.Selecao;
 import br.ufc.npi.auxilio.model.Servidor;
 import br.ufc.npi.auxilio.model.VisitaDomiciliar;
+import br.ufc.npi.auxilio.repository.VisitaDomiciliarRepository;
 import br.ufc.npi.auxilio.service.AlunoService;
 import br.ufc.npi.auxilio.service.InscricaoService;
 import br.ufc.npi.auxilio.service.ServidorService;
@@ -36,6 +36,9 @@ public class VisitaController {
 	@Autowired
 	private ServidorService servidorService;
 	
+	@Autowired
+	private VisitaDomiciliarRepository visitaDomiciliarRepository;
+	
 	@PreAuthorize(PERMISSAO_COORDENADOR)
 	@GetMapping("/cadastrar/{inscricao}")
 	public String cadastrarVisitaForm(@PathVariable Inscricao inscricao, Authentication auth, Model model){
@@ -43,21 +46,25 @@ public class VisitaController {
 		Servidor servidor =  servidorService.getByCpf(auth.getName());
 		
 		Inscricao inscricaoBanco = inscricaoService.get(aluno, inscricao.getSelecao());
+		model.addAttribute("servidor", servidor);
 		model.addAttribute("inscrição", inscricaoBanco != null ? inscricaoBanco.getId() : null);
 		model.addAttribute("selecao", inscricao.getSelecao());
-		model.addAttribute("resultados", Resultado.values());
 		model.addAttribute("visita", new VisitaDomiciliar());
 		model.addAttribute("tiposDeDocumento", inscricao.getSelecao().getDocumentos());
 		return "selecao/visita-domiciliar";
 	}
 	
 	@PreAuthorize(PERMISSAO_COORDENADOR)
-	@PostMapping("/cadastrar/{inscricao}")
-	public String cadastrarVisita(Model model) {
+	@PostMapping("/cadastrar")
+	public String cadastrarVisita(VisitaDomiciliar visitaDomiciliar, Model model, Authentication auth) {
 		model.addAttribute("acao", "cadastrar");
-		model.addAttribute("opcoesTipoSelecao", TipoSelecao.values());
-		model.addAttribute("selecao", new Selecao());
-		
-		return "selecao/cadastrar-visita";
+		visitaDomiciliar.setResponsavel(servidorService.getByCpf(auth.getName()));
+		this.visitaDomiciliarRepository.save(visitaDomiciliar);
+		return "redirect:/selecao/";
+	}
+	
+	@ModelAttribute("resultados")
+	public Resultado[] getResultados() {
+		return Resultado.values();
 	}
 }
