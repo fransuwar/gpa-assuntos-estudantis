2a60fd334d6549d2b578962ae14db79bc5d7a581
Joao Victor <vitorufc2013@gmail.com>
2017-06-27 16:51:12 -0300

    Correcao

--- auxilio-moradia/src/main/resources/static/js/auxilio/selecao/pagamentos.js
+++ auxilio-moradia/src/main/resources/static/js/auxilio/selecao/pagamentos.js
@@ -3,52 +3,28 @@ $(document).ready(function(){
         "scrollX": true,
         "paging": false,
         "ordering": false,
-        dom: 'Blrtp',
+        dom: "Blrtp",
         buttons: [
             {
             	text: '<i class="material-icons">payment</i>',
-            	extend: 'pdfHtml5',
-            	title: 'Lista - Pagamento',
-                orientation: 'portrait',
-                pageSize: 'A4',
-                className: 'btn-floating deep-purple lighten-1 tooltipped',
+            	extend: "pdfHtml5",
+            	title: "Lista - Pagamento",
+                orientation: "portrait",
+                pageSize: "A4",
+                className: "btn-floating deep-purple lighten-1 tooltipped",
             }
         ],
-        /*
-        "language": {
-            "sEmptyTable": "Nenhum registro encontrado",
-            "sInfo": "Mostrando de _START_ até _END_ de _TOTAL_ registros",
-            "sInfoEmpty": "Mostrando 0 até 0 de 0 registros",
-            "sInfoFiltered": "(Filtrados de _MAX_ registros)",
-            "sInfoPostFix": "",
-            "sInfoThousands": ".",
-            "sLengthMenu": "_MENU_ resultados por página",
-            "sLoadingRecords": "Carregando...",
-            "sProcessing": "Processando...",
-            "sZeroRecords": "Nenhum registro encontrado",
-            "sSearch": "Pesquisar",
-            "oPaginate": {
-                "sNext": "Próximo",
-                "sPrevious": "Anterior",
-                "sFirst": "Primeiro",
-                "sLast": "Último"
-            },
-            "oAria": {
-                "sSortAscending": ": Ordenar colunas de forma ascendente",
-                "sSortDescending": ": Ordenar colunas de forma descendente"
-            }
-        }*/
     });
     //Alocação dos botões do dataTables dentro de um FAB do materialize
     if(table.buttons){
     	$(".dt-button").removeClass("dt-button");
-        var botoes = table.buttons().container().children();
+        var botoes=table.buttons().container().children();
         botoes.detach();
-        botoes.each(function (){
-        	$(this).attr("data-position", "left");
-        	$(this).attr("data-delay", "50");
+        botoes.each(function(){
+        	$(this).attr("data-position","left");
+        	$(this).attr("data-delay","50");
         	if($(this).hasClass("buttons-pdf")){
-        		$(this).attr("data-tooltip", "Exportar para pagamento");            	
+        		$(this).attr("data-tooltip","Exportar para pagamento");            	
         	}
         	var tag = $("<li></li>");
         	$(this).appendTo(tag);
