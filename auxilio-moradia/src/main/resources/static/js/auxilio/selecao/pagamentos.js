204a72e3966a9a6fc0836b49b659328808da99ee
Joao Victor <vitorufc2013@gmail.com>
2017-06-27 16:58:59 -0300

    Correcao 2

--- auxilio-moradia/src/main/resources/static/js/auxilio/selecao/pagamentos.js
+++ auxilio-moradia/src/main/resources/static/js/auxilio/selecao/pagamentos.js
@@ -1,35 +1,35 @@
 $(document).ready(function(){
 	var table = $("#pagamento").DataTable({
-        "scrollX": true,
-        "paging": false,
-        "ordering": false,
-        dom: "Blrtp",
-        buttons: [
-            {
-            	text: '<i class="material-icons">payment</i>',
-            	extend: "pdfHtml5",
-            	title: "Lista - Pagamento",
-                orientation: "portrait",
-                pageSize: "A4",
-                className: "btn-floating deep-purple lighten-1 tooltipped",
-            }
-        ],
-    });
-    //Alocação dos botões do dataTables dentro de um FAB do materialize
-    if(table.buttons){
-    	$(".dt-button").removeClass("dt-button");
-        var botoes=table.buttons().container().children();
-        botoes.detach();
-        botoes.each(function(){
-        	$(this).attr("data-position","left");
-        	$(this).attr("data-delay","50");
-        	if($(this).hasClass("buttons-pdf")){
-        		$(this).attr("data-tooltip","Exportar para pagamento");            	
-        	}
-        	var tag = $("<li></li>");
-        	$(this).appendTo(tag);
-        	tag.appendTo("#float_relatorio");
-        })            
-        $('.tooltipped').tooltip();
+"scrollX": true,
+"paging": false,
+"ordering": false,
+dom: "Blrtp",
+buttons: [
+{
+	text: '<i class="material-icons">payment</i>',
+	extend: "pdfHtml5",
+	title: "Lista - Pagamento",
+	orientation: "portrait",
+	pageSize: "A4",
+className: "btn-floating deep-purple lighten-1 tooltipped",
+        }
+    ],
+});
+//Alocação dos botões do dataTables dentro de um FAB do materialize
+if(table.buttons){
+	$(".dt-button").removeClass("dt-button");
+var botoes=table.buttons().container().children();
+botoes.detach();
+botoes.each(function(){
+	$(this).attr("data-position","left");
+$(this).attr("data-delay","50");
+if($(this).hasClass("buttons-pdf")){
+$(this).attr("data-tooltip","Exportar para pagamento");            	
+}
+var tag = $("<li></li>");
+$(this).appendTo(tag);
+tag.appendTo("#float_relatorio");
+});           
+$(".tooltipped").tooltip();
     }
 });
