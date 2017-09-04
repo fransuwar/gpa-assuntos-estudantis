3759805a0bc355d0e5a12e5d59da539b833d0e6b
Allan Vidal <allanfvc@gmail.com>
2017-09-04 17:16:31 -0300

    Correções codacy

--- auxilio-moradia/src/main/resources/static/js/auxilio/selecao/entrevista.js
+++ auxilio-moradia/src/main/resources/static/js/auxilio/selecao/entrevista.js
@@ -2,17 +2,12 @@ $(document).ready(function(){
 	
 });
 
-$(".filtrar-cursos").on("click", ".filtro-curso",function(){
-	let seletor = $(".alunos ul").children();
-	let opcoes = $("select.alunos option");
-	let filtros = $.map($("input:checked"), function(c){return c.id;});
-	if(filtros.length == 0){
-		seletor.removeAttr("hidden");
-	}
-	else{
-		ocultar(opcoes, seletor,filtros);
-	}	
-});
+function atualizarSeletor(seletor, opcao){	
+	let exibivel = $(".alunos ul").parent().find("input");
+	let filtrados = $.map($(seletor), function(c){ if(!$(c).prop("hidden")) return $(c).text();});
+	exibivel.val(filtrados[0]);
+	$("select.alunos").val($(opcao).val());
+}
 
 function ocultar(opcoes, seletor,filtros){
 	opcoes.removeAttr("hidden");
@@ -21,7 +16,7 @@ function ocultar(opcoes, seletor,filtros){
 		let opcao = opcoes[j];
 		let existeAluno = false;
 		for(let i=0; i<filtros.length; i++){
-			if(opcao.id==filtros[i].toString()) existeAluno = true;
+			if(opcao.id===filtros[i].toString()) existeAluno = true;
 		}
 		if(!existeAluno){
 			$(seletor[j]).attr("hidden","");			
@@ -33,11 +28,31 @@ function ocultar(opcoes, seletor,filtros){
 	atualizarSeletor($(seletor), primeiraOpcao);
 }
 
-function atualizarSeletor(seletor, opcao){	
-	let exibivel = $(".alunos ul").parent().find("input");
-	let filtrados = $.map($(seletor), function(c){ if(!$(c).prop("hidden")) return $(c).text() });
-	exibivel.val(filtrados[0]);
-	$('select.alunos').val($(opcao).val());
+$(".filtrar-cursos").on("click", ".filtro-curso",function(){
+	let seletor = $(".alunos ul").children();
+	let opcoes = $("select.alunos option");
+	let filtros = $.map($("input:checked"), function(c){return c.id;});
+	if(filtros.length === 0){
+		seletor.removeAttr("hidden");
+	}
+	else{
+		ocultar(opcoes, seletor,filtros);
+	}	
+});
+
+function adicionarAluno(result, form){
+	let chip = 	"<div class='chip deep-purple lighten-1'>" +
+					"<div class='text-17 light white-text'>"+result.nome+"</div>" +
+					"<a title='Excluir' href='/selecao/agendamentoEntrevista/"+result.agendamento+"/excluir/"+result.inscricao+"'>" +
+					"<i class='close white-text material-icons'>close</i></a>" +
+				"</div>";
+	let select = $("select.alunos").find("option[value='"+result.inscricao+"']");
+	let parent = form.parent().find(".center");
+	parent.append(chip);
+	select.remove();
+	$(".alunos").material_select("destroy");
+	$(".alunos").material_select();
+			
 }
 
 $("form#agendamento-alunos").on("submit",function(event){
@@ -48,7 +63,6 @@ $("form#agendamento-alunos").on("submit",function(event){
 	var url = "/selecao/agendamentoEntrevista/adicionar";
 	var token = $("meta[name='_csrf']").attr("content");
 	var id =$(this).attr("id");
-	var hab = $(this).is(":checked");
 	var param = {idAgendamento : agendamento, idInscricao : select };
 	$.ajax({
 		url,
@@ -60,28 +74,12 @@ $("form#agendamento-alunos").on("submit",function(event){
 		success: function(response) {			
 			if(response.status === "DONE"){
 				adicionarAluno(JSON.parse(response.object), form);
-				Materialize.toast(response.alert.message, response.alert.delay, "green rounded")
+				Materialize.toast(response.alert.message, response.alert.delay, "green rounded");
 			}else{
-				Materialize.toast(response.alert.message, response.alert.delay, "red rounded")
+				Materialize.toast(response.alert.message, response.alert.delay, "red rounded");
 			}
 		}
 	});
 
 });
-
-function adicionarAluno(result, form){
-	let chip = 	"<div class='chip deep-purple lighten-1'>" +
-					"<div class='text-17 light white-text'>"+result.nome+"</div>" +
-					"<a title='Excluir' href='/selecao/agendamentoEntrevista/"+result.agendamento+"/excluir/"+result.inscricao+"'>" +
-					"<i class='close white-text material-icons'>close</i></a>" +
-				"</div>"
-	let select = $("select.alunos").find("option[value='"+result.inscricao+"']");
-	let div = $("div.alunos")
-	let parent = form.parent().find(".center");
-	parent.append(chip);
-	select.remove();
-	$('.alunos').material_select('destroy');
-	$('.alunos').material_select();
-			
-}
 
