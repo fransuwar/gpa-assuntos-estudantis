7ad8cff6cdc5c47d31778aacbbd3926eb78118b2
Joao Vitor Chaves <mrjvco@gmail.com>
2017-09-19 17:05:01 -0300

    Commit temporário

--- auxilio-moradia/src/main/java/br/ufc/npi/auxilio/controller/InscricaoController.java
+++ auxilio-moradia/src/main/java/br/ufc/npi/auxilio/controller/InscricaoController.java
@@ -12,6 +12,7 @@ import br.ufc.npi.auxilio.service.*;
 import br.ufc.npi.auxilio.utils.api.Response;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
@@ -19,6 +20,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import static br.ufc.npi.auxilio.utils.Constants.COORDENADOR;
 import static br.ufc.npi.auxilio.utils.Constants.ERRO;
 import static br.ufc.npi.auxilio.utils.Constants.INFO;
 import static br.ufc.npi.auxilio.utils.Constants.PERMISSAO_ALUNO;
@@ -420,6 +422,21 @@ public class InscricaoController {
 			return new Response().withFailStatus().withErrorMessage("Error ao selecionar esta inscricao");
 	}
 	
+	
+	
+	@Secured(COORDENADOR)
+	@PostMapping("/parecerFinal")
+	@ResponseBody
+	public Response editarParecerFinal(Integer inscricaoId,
+			String resultado, String observacao) throws AuxilioMoradiaException{
+		Inscricao insc = inscricaoService.buscarInscricaoPorId(inscricaoId);
+		insc.setObservacao(observacao);
+		insc.setResultado(Resultado.valueOf(resultado));
+		inscricaoService.editar(insc);
+		return new Response().withDoneStatus();
+	}
+	
+
 	@PreAuthorize(PERMISSAO_COORDENADOR)
 	@GetMapping("/resultadoSelecao/{selecao}")
 	public String resultadoSelecao(@PathVariable Selecao selecao, Model model){
--- auxilio-moradia/src/main/resources/static/js/auxilio/inscricao/selecionar_inscricao.js
+++ auxilio-moradia/src/main/resources/static/js/auxilio/inscricao/selecionar_inscricao.js
@@ -2,8 +2,9 @@ $(document).ready(function() {
 	$( ".selecionar" ).click(function(event) {
 		event.preventDefault();
 		var checkbox = $(this);
-		var url = "/inscricao/selecionar";
+		var _context = $("meta[name='_context']").attr("content");
 		var token = $("meta[name='_csrf']").attr("content");
+		var url = _context+"/inscricao/selecionar";
 		var id =$(this).attr("id");
 		var hab = $(this).is(":checked");
 		var param = {idInscricao : id, selecionar : hab };
@@ -17,9 +18,9 @@ $(document).ready(function() {
 			success: function(response) {
 				if(response.status === "DONE"){
 					if(hab){ 
-						checkbox.prop("checked", true);
+						checkbox.prop("checked", true); 
 					}else{
-						checkbox.prop("checked", false); 	
+						checkbox.prop("checked", false);  	
 					}
 				}
 			}
@@ -28,18 +29,47 @@ $(document).ready(function() {
 	
 });
 
+
 $(document).on("click", ".alterarParecer", function () {
     $("#nomeAluno").text("Aluno(a): "+$(this).data("text"));
     $("#disabled").val($(this).data("id"))
 });
 
 $("#modal1 form").on("submit", function(event){
-	event.preventDefault();
-	let inscricaoId = $("#disabled").val();
+	event.preventDefault();	
+	let _context = $("meta[name='_context']").attr("content");
+	let token = $("meta[name='_csrf']").attr("content");
+	let id = $("#disabled").val();
 	let resultado = $(this).find("select").val();
 	let observacao = $(this).find("#observacao").val();
-	
-	console.log(inscricaoId, resultado, observacao);
+	let url = _context+"/inscricao/parecerFinal";
+	let selecionar = $($(".alterarParecer[data-id='"+id+"']").parent().parent());
+	var param = {inscricaoId : id, resultado : resultado, observacao : observacao};
+	let button = $(".alterarParecer[data-id='"+id+"']").find("#inscResult");
+	$.ajax({
+		url,
+		type: "post",
+		dataType: "json",
+		data: param,
+		headers: {"X-CSRF-TOKEN":token},
+		
+		success: function(response) {
+			button.removeClass('red green grey');
+			if(response.status === "DONE"){
+				console.log(selecionar)
+				if(param.resultado == "DEFERIDO"){
+					button.text('Deferido');
+					button.addClass('green');
+				}else if(param.resultado == "INDEFERIDO"){
+					button.text('Indeferido');
+					button.addClass('red');
+				}else{
+					button.text('Não Avaliado');
+					button.addClass('grey');
+				}
+			}
+			$('.modal').modal('close');
+		}
+	});
 });
-
 
