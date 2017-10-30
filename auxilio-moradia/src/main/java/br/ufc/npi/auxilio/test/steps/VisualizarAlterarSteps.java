a1715cba682d2c5e991b90698c1599aa291928bf
amarildobarros <amarildobarrosV@gmail.com>
2017-10-30 11:57:04 -0300

    ainda faltam 4 cenários, mas está praticamente concluido.

--- auxilio-moradia/src/main/java/br/ufc/npi/auxilio/test/steps/VisualizarAlterarSteps.java
+++ auxilio-moradia/src/main/java/br/ufc/npi/auxilio/test/steps/VisualizarAlterarSteps.java
@@ -9,18 +9,23 @@ import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import br.ufc.npi.auxilio.test.pages.LoginPage;
 
 public class VisualizarAlterarSteps {
-private WebDriver driver;
+	 
+	private WebDriver driver;
 	
-	@BeforeStories
-	public void acessar() {
+	public VisualizarAlterarSteps(){
 		System.setProperty("webdriver.firefox.bin", "/home/amarildo.junior/Downloads/firefox/firefox");
 		driver = new FirefoxDriver();
 		driver.get("http://localhost:8080/login");
+	}
+	
+	@BeforeStories
+	public void acessar() {
 		//cenários iniciam com o usuário já logado
 		LoginPage acesso = new LoginPage(driver);
 		acesso.logar();
@@ -28,12 +33,12 @@ private WebDriver driver;
 	
 	@Given("um servidor visualize inscrições de uma seleção")
 	public void paginaVisualizarIncricoes(){
-		driver.get("http://localhost:8080/selecao/inscricoes/7");
+		driver.get("http://localhost:8080/selecao/inscricoes/52");
 	}
 	
-	@Given("um servidor clique no botão referente ao formulário de parecer final")
+	@Given("um servidor possa clicar no botão referente ao formulário de parecer final")
 	public void clicarBotaoParcerFinal(){
-		driver.findElement(By.id("inscResult"));
+		driver.get("http://localhost:8080/selecao/inscricoes/52");
 	}
 
 	@Given("um servidor clique em parecer final")
@@ -57,85 +62,88 @@ private WebDriver driver;
 		driver.findElement(By.id("modal1"));
 	}
 	
-	@When("o resultado de analiseDocumentação seja deferido")
-	public void resultadoDeAnaliseDocumentacaoDeferido(){
-		driver.findElement(By.id("inscDocumentacao"));
-		driver.findElement(By.linkText("Deferido"));
-	}
-	
-	@When("entrevista seja deferido")
-	public void resultadoDeEntrevistaDeferido(){
-		driver.findElement(By.id("inscEntrevista"));
-		driver.findElement(By.linkText("Deferido"));
-	}
-	
-	@When("visitaDomiciliar seja deferido")
-	public void resultadoDeVisitaDomiciliarDeferido(){
-		driver.findElement(By.id("inscVisita"));
-		driver.findElement(By.linkText("Deferido"));
-	}
-	
-	@When("o resultado de AnaliseDocumentação seja indeferido Ou entrevista seja indeferido Ou visitaDomiciliar seja indeferido")
-	public void umDos3ResultadosIndeferido(){
+	@When("o resultado de Análise Documentação, Entrevista e Visita Domiciliar forem $valor")
+	public void valorDeferido(String valor) {
+		WebElement documentacao = driver.findElement(By.xpath(".//a[@href='/documentacao/inscricao/27']/div[@id='inscDocumentacao']"));
+		WebElement entrevista = driver.findElement(By.xpath(".//a[@href='/entrevista/27']/div[@id='inscEntrevista']"));
+		WebElement visita = driver.findElement(By.xpath(".//a[@href='/visita/27']/div[@id='inscVisita']"));
 		
+		assertEquals(documentacao.getText(), valor);
+		assertEquals(entrevista.getText(), valor);
+		assertEquals(visita.getText(), valor);
 	}
 	
-	@When("o resultado de AnaliseDocumentação seja não avaliado Ou Entrevista seja não avaliado Ou VisitaDomiciliar seja não avaliado")
-	public void umDos3ResultadosNaoAvaliados(){
-
+	@When("o resultado de $documentação seja $valor")
+	public void umDos3Resultados(String documentacao, String valor){
+		WebElement iddoc;
+		switch(documentacao){
+			case "AnaliseDocumentacao":
+				iddoc= driver.findElement(By.xpath("//a[@href='/documentacao/inscricao/28']/div[@id='inscDocumentacao']"));
+				assertEquals(iddoc.getText(), valor);
+				break;
+			case "Entrevista":
+				iddoc= driver.findElement(By.xpath("//a[@href='/entrevista/28']/div[@id='inscEntrevista']"));
+				assertEquals(iddoc.getText(), valor);
+				break;
+			case "VisitaDomiciliar":
+				iddoc= driver.findElement(By.xpath("//a[@href='/visita/28']/div[@id='inscVisita']"));
+				assertEquals(iddoc.getText(), valor);
+				break;
+		}
 	}
 	
 	@When("o resultado do parecer final seja deferido")
 	public void botaoParecerFinalDeferido(){
-		driver.findElement(By.id("inscResult"));
-		driver.findElement(By.linkText("Deferido"));
+		WebElement botaoParecerFinal = driver.findElement(By.xpath(".//a[@data-id='27']/div[@id='inscResult']"));
+		assertEquals(botaoParecerFinal.getText(), "Deferido");
 	}
 	
 	@When("o resultado do parecer final seja indeferido")
 	public void botaoParecerFinalIndeferido(){
-		driver.findElement(By.id("inscResult"));
-		driver.findElement(By.linkText("Indeferido"));
+		WebElement botaoParecerFinal = driver.findElement(By.xpath(".//a[@data-id='28']/div[@id='inscResult']"));
+		assertEquals(botaoParecerFinal.getText(), "Indeferido");
 	}
 	
 	@When("o resultado da AnaliseDocumentação seja deferido")
 	public void botaoAnaliseDocumentacaoDeferido(){
-		driver.findElement(By.id("inscDocumentacao"));
-		driver.findElement(By.linkText("Deferido"));
+		WebElement botaoDocumentacao = driver.findElement(By.xpath("//a[@href='/documentacao/inscricao/27']/div[@id='inscDocumentacao']"));
+		assertEquals(botaoDocumentacao.getText(), "Deferido");
 	}
 	
 	@When("o resultado da AnaliseDocumentação seja indeferido")
 	public void botaoAnaliseDocumentacaoIndeferido(){
-		driver.findElement(By.id("inscDocumentacao"));
-		driver.findElement(By.linkText("Indeferido"));
+		WebElement botaoDocumentacao = driver.findElement(By.xpath("//a[@href='/documentacao/inscricao/26']/div[@id='inscDocumentacao']"));
+		assertEquals(botaoDocumentacao.getText(), "Indeferido");
 	}
 	
 	@When("o resultado da Entrevista seja deferido")
 	public void botaoEntrevistaDeferido(){
-		driver.findElement(By.id("inscEntrevista"));
-		driver.findElement(By.linkText("Deferido"));
+		WebElement botaoEntrevista = driver.findElement(By.xpath("//a[@href='/entrevista/27']/div[@id='inscEntrevista']"));
+		assertEquals(botaoEntrevista.getText(), "Deferido");
 	}
 	
 	@When("o resultado da Entrevista seja indeferido")
 	public void botaoEntrevistaIndeferido(){
-		driver.findElement(By.id("inscEntrevista"));
-		driver.findElement(By.linkText("Indeferido"));
+		WebElement botaoEntrevista = driver.findElement(By.xpath("//a[@href='/entrevista/28']/div[@id='inscEntrevista']"));
+		assertEquals(botaoEntrevista.getText(), "Indeferido");
 	}
 	
 	@When("o resultado da VisitaDomiciliar seja deferido")
 	public void botaoVisitaDomiciliarDeferido(){
-		driver.findElement(By.id("inscVisita"));
-		driver.findElement(By.linkText("Deferido"));
+		WebElement botaoVisita = driver.findElement(By.xpath("//a[@href='/visita/27']/div[@id='inscVisita']"));
+		assertEquals(botaoVisita.getText(), "Deferido");
 	}
 	
 	@When("o resultado da VisitaDomiciliar seja indeferido")
 	public void botaoVisitaDomiciliarIndeferido(){
-		driver.findElement(By.id("inscVisita"));
-		driver.findElement(By.linkText("Indeferido"));
+		WebElement botaoVisita = driver.findElement(By.xpath("//a[@href='/visita/28']/div[@id='inscVisita']"));
+		assertEquals(botaoVisita.getText(), "Indeferido");
 	}
 	
 	@When("o botão referente ao formulário de parecer final é clicado")
 	public void botaoParecerFinalClicado(){
-		driver.findElement(By.id("inscResult")).click();
+		WebElement botaoParecer = driver.findElement(By.xpath(".//div[@id='inscResult']"));
+		botaoParecer.click();
 	}
 	
 	@When("o parecer final é clicado")
@@ -158,30 +166,41 @@ private WebDriver driver;
 		
 	}
 	
-	@Then("o resultado de parecer final seja deferido")
-	public void resultadoParecerFinalDeferido(){
-		assertEquals(driver.findElement(By.id("inscResult")), driver.findElement(By.linkText("deferido")));
-		//driver.findElement(By.id("inscResult"));
-		//driver.findElement(By.linkText("deferido"));
-	}
 	
-	@Then("o resultado de parecer final seja indeferido")
-	public void resultadoParecerFinalIndeferido(){
-	
-	}
-	
-	@Then("a cor do ícone deve ser verde")
-	public void corVerde(){
+	@Then("o resultado do Parecer Final será $valorParecer")
+	public void resultadoParecerFinal(String valorParecer){
+		WebElement idParecer;
 		
+		switch(valorParecer){
+			case "Deferido":
+				idParecer = driver.findElement(By.xpath(".//a[@data-id='27']/div[@id='inscResult']"));
+				assertEquals(idParecer.getText(), valorParecer);
+				break;
+			case "Indeferido":
+				idParecer = driver.findElement(By.xpath(".//a[@data-id='28']/div[@id='inscResult']"));
+				assertEquals(idParecer.getText(), valorParecer);
+				break;
+		}
 	}
 	
-	@Then("a cor do ícone deve ser vermelho")
-	public void corVermelho(){
-		
+	@Then("a cor do ícone será $verde")
+	public void corVerde(String verde){
+		WebElement botao = driver.findElement(By.xpath(".//div[@class='chip green lighten-1']"));
+		WebElement cor = driver.findElement(By.cssSelector(".green.lighten-1"));
+		assertEquals(botao, cor);
 	}
 	
+	@Then("a cor do ícone deve ser $vermelho")
+	public void corVermelho(String vermelho){
+		WebElement botao = driver.findElement(By.xpath(".//div[@class='chip red lighten-1']"));
+		WebElement cor = driver.findElement(By.cssSelector(".red.lighten-1"));
+		assertEquals(botao, cor);
+	}
+	//ainda não está passando por aqui
 	@Then("uma tela é exibida referente ao formulário de parecer final possibilitando alterações sobre o mesmo")
 	public void exibirFormulario(){
+		WebElement modal = driver.findElement(By.xpath(".//div[@id='modal1']"));
+		assertEquals(modal.getClass(), "modal open");
 		
 	}
 	
--- auxilio-moradia/src/main/java/br/ufc/npi/auxilio/test/story/VisualizarAlterar.story
+++ auxilio-moradia/src/main/java/br/ufc/npi/auxilio/test/story/VisualizarAlterar.story
@@ -1,89 +1,113 @@
 Story: Como servidor gostaria de visualizar/alterar o parecer final da seleção.
 
+@Meta
+@Autor: Amarildo Barros
+@Data: 19/10/2017
+
 Cenário: O Parecer final deferido
 Dado que um servidor visualize inscrições de uma seleção
-Quando o resultado de analiseDocumentação seja deferido
-E entrevista seja deferido
-E visitaDomiciliar seja deferido
-Então o resultado de parecer final seja deferido
+Quando o resultado de Análise Documentação, Entrevista e Visita Domiciliar forem <valor>
+Então o resultado do Parecer Final será <valorParecer>
+
+Exemplos:
+|valor|valorParecer|
+|Deferido|Deferido|
 
 Cenário: O Parecer final indeferido
 Dado que um servidor visualize inscrições de uma seleção
-Quando o resultado de AnaliseDocumentação seja indeferido
-Ou entrevista seja indeferido
-Ou visitaDomiciliar seja indeferido
-Então o resultado de parecer final seja indeferido
+Quando o resultado de <documentação> seja <valor>
+Então o resultado do Parecer Final será <valorParecer>
+
+Exemplos:
+|documentação|valor|valorParecer|
+|AnaliseDocumentacao|Indeferido|Indeferido|
+|Entrevista|Indeferido|Indeferido|
+|VisitaDomiciliar|Indeferido|Indeferido|
 
 Cenário: O Parecer final quando um dos resultados ainda não foi avaliado
 Dado que um servidor visualize inscrições de uma seleção
-Quando o resultado de AnaliseDocumentação seja não avaliado
-Ou Entrevista seja não avaliado
-Ou VisitaDomiciliar seja não avaliado
-Então o resultado de parecer final seja indeferido
+Quando o resultado de <documentação> seja <valor>
+Então o resultado do Parecer Final será <valorParecer>
+
+Exemplos:
+|documentação|valor|valorParecer|
+|AnaliseDocumentacao|Não Avaliado|Indeferido|
+|Entrevista|Não Avaliado|Indeferido|
+|VisitaDomiciliar|Não Avaliado|Indeferido|
 
 Cenário: Quando o ícone é verde para parecer final
 Dado que um servidor visualize inscrições de uma seleção
 Quando o resultado do parecer final seja deferido
-Então a cor do ícone deve ser verde
+Então a cor do ícone será <verde>
+
+Exemplos:
+|verde|
+|#66BB6A|
 
 Cenário: Quando o ícone é vermelho para parcer final
 Dado que um servidor visualize inscrições de uma seleção
 Quando o resultado do parecer final seja indeferido
-Então a cor do ícone deve ser vermelho
+Então a cor do ícone deve ser <vermelho>
+
+Exemplos:
+|vermelho|
+|#EF5350|
 
 Cenário: Quando o ícone é verde para resultado de AnaliseDocumentação
 Dado que um servidor visualize inscrições de uma seleção
 Quando o resultado da AnaliseDocumentação seja deferido
-Então a cor do ícone deve ser verde
+Então a cor do ícone será <verde>
+
+Exemplos:
+|verde|
+|#66BB6A|
 
 Cenário: Quando o ícone é vermelho para resultado de AnaliseDocumentação
 Dado que um servidor visualize inscrições de uma seleção
 Quando o resultado da AnaliseDocumentação seja indeferido
-Então a cor do ícone deve ser vermelho
+Então a cor do ícone deve ser <vermelho>
+
+Exemplos:
+|vermelho|
+|#EF5350|
 
 Cenário: Quando o ícone é verde para resultado de Entrevista
 Dado que um servidor visualize inscrições de uma seleção
 Quando o resultado da Entrevista seja deferido
-Então a cor do ícone deve ser verde
+Então a cor do ícone será <verde>
+
+Exemplos:
+|verde|
+|#66BB6A|
 
 Cenário: Quando o ícone é vermelho para resultado de Entrevista
 Dado que um servidor visualize inscrições de uma seleção
 Quando o resultado da Entrevista seja indeferido
-Então a cor do ícone deve ser vermelho
+Então a cor do ícone deve ser <vermelho>
+
+Exemplos:
+|vermelho|
+|#EF5350|
 
 Cenário: Quando o ícone é verde para resultado de VisitaDomiciliar
 Dado que um servidor visualize inscrições de uma seleção
 Quando o resultado da VisitaDomiciliar seja deferido
-Então a cor do ícone deve ser verde
+Então a cor do ícone será <verde>
+
+Exemplos:
+|verde|
+|#66BB6A|
 
 Cenário: Quando o ícone é vermelho para resultado de VisitaDomiciliar
 Dado que um servidor visualize inscrições de uma seleção
 Quando o resultado da VisitaDomiciliar seja indeferido
-Então a cor do ícone deve ser vermelho
+Então a cor do ícone deve ser <vermelho>
+
+Exemplos:
+|vermelho|
+|#EF5350|
 
 Cenário: Quando exibir o formulário do parecer final
-Dado que um servidor clique no botão referente ao formulário de parecer final
+Dado que um servidor possa clicar no botão referente ao formulário de parecer final
 Quando o botão referente ao formulário de parecer final é clicado
 Então uma tela é exibida referente ao formulário de parecer final possibilitando alterações sobre o mesmo
-
-Cenário: Quando não exibir o formulário do parecer final
-Dado que um servidor clique em parecer final
-Quando o parecer final é clicado
-Então nada é para ser exibido além do que já estava sendo exibido
-
-Cenário: Quando o formulário de parcer final for exibido
-Dado que servidor deseje visualizar o formulário referente ao de parecer final
-Quando o formulário é exibido
-Então os campos de edição que o servidor pode editar são resultado e observações
-
-Cenário: Quando é feita uma alteração no campo de observações no formulário de parecer final
-Dado que um servidor deseje alterar os dados do campo de observações do formulário
-Quando as alterações no campo de observações é feita e o botão de salvar é clicado
-Então o campo do formulário referente a observações é alterado
-E a tela de inscições para aquela seleção é mostrada novamente
-
-Cenário: Quando é feita uma alteração no campo de resultado no formulário de parecer final
-Dado que um servidor deseje alterar os dados do campo de resultado do formulário
-Quando a alteração no campo de resultado é feita e o botão de salvar é clicado
-Então o campo do formulário referente a resultado não é alterado
-E a tela de inscições para aquela seleção é mostrada novamente
